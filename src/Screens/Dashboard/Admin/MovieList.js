67fc5b5946558cc7acc8165b078b50fbbc366ef6
Rakitha <tharupathir@gmail.com>
2024-05-17 22:33:18 +0530

    Update MovieList.js

--- src/Screens/Dashboard/Admin/MovieList.js
+++ src/Screens/Dashboard/Admin/MovieList.js
@@ -7,7 +7,7 @@ function MoviesList(){
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        fetch('http://lobster-app-bxg93.ondigitalocean.app/movies')
+        fetch('https://lobster-app-bxg93.ondigitalocean.app/movies')
             .then(response => response.json())
             .then(data => {
                 const mappedMovies = data.map(movie => ({
